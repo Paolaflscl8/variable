715d8bf51bec49d4e50705eff8fe325149db5b98
Paolaflscl8 <113227120+Paolaflscl8@users.noreply.github.com>
2022-10-03 22:50:28 -0600

    Add files via upload

---
+++ varialbes.java
@@ -0,0 +1,13 @@
+public class varialbes {
+    public static void main(String[] args) {
+        int par = 50;
+        int base = 50;
+
+        if (par == base){
+            for (int i = 1; i <= base; i++){
+                System.out.print(i + "-");
+            }
+        }else{
+            System.out.println("No retornar el numero");}
+    }
+}
